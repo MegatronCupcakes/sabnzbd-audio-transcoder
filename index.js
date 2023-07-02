9fddc7f546452855be8d69eccb443c11d7b059b0
Dallas Howard <dallas@galacticwidgets.com>
2023-07-02 14:10:58 -0400

    updated error logging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/transcode', (request, response) => {
         }
     })
     .catch((error) => {
-        response.status(500).send(`transcoding error! transcoding failed :-(\n(${error.message})\n`);
+        response.status(500).send(`transcoding error! transcoding failed :-(\n(${error.message ? error.message : error})\n`);
     });
 });
 
